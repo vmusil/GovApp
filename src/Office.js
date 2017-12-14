9a2861abc3f778524e807875325ecc1b14e018db
Vitezslav Musil <vitez.7@seznam.cz>
2017-12-14 20:24:25 +0100

    Added static maps.

--- src/Office.js
+++ src/Office.js
@@ -5,12 +5,15 @@ class Office extends React.PureComponent{
     render() {
         console.log(this.props)
         const {...data} = this.props
+        const mapUrl = `https://maps.googleapis.com/maps/api/staticmap?size=640x400&center=${data.latitude},${data.longitude}&zoom=15&markers=${data.latitude},${data.longitude}`
 
         return (<div>
             {data.name} <br/>
             {data.addressStreet}, {data.addressCity} <br/>
             {data.officeHours} <br/>
-            <a href={data.www} >{data.www}</a>
+            {data.telefon.telCislo} <br/>
+            <a href={data.www} >{data.www}</a> <br/>
+            <img src={mapUrl} /> <br/>
             
             </div>)
     
